8e993495ab0eb770cd4ed1ae518816f7020c3ca3
Lee-Jiseung <ljss98@naver.com>
2020-04-29 10:21:40 +0900

    num2

--- 201711417-MidTerm/src/main/java/ku/opensrcsw/_MidTerm/App.java
+++ 201711417-MidTerm/src/main/java/ku/opensrcsw/_MidTerm/App.java
@@ -6,11 +6,11 @@ import java.text.ParseException;
 
 import org.json.simple.*;
 import org.json.simple.parser.*;
-
+import midterm.problem2.RegularExpression;
 
 /**
  * Hello world!
- *
+ *t
  */
 
 
@@ -22,18 +22,23 @@ public class App
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
         
-        
+        RegularExpression re = new RegularExpression();
      
     	JSONParser jsonParser = new JSONParser();
     	Object element = jsonParser.parse(new FileReader("midterm.json"));
     	JSONObject jsonObject = (JSONObject) element;
     	JSONArray infoArray = (JSONArray) jsonObject.get("poem");
-    	   
+    	
     	for(int i=0; i<infoArray.size(); i++) {
     		JSONObject itemObject = (JSONObject) infoArray.get(i);
+    		
     		String str = itemObject.toJSONString();
-    		if(str.contains(input))	
+    		//if(str.contains(input))	
+    			//System.out.println("item"+i+ " :"+itemObject.get("item"));
+    		if(re.check(str, input)) {
     			System.out.println("item"+i+ " :"+itemObject.get("item"));
+    		}
+    		
     	}
         
        
